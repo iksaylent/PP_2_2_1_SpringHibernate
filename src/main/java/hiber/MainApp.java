b23233e7b9459d84a8ffaf1dae76255e220dee5f
Andrey Vachkov <iksaylent@yandex.ru>
2022-11-30 20:54:32 +0500

    Проект обновлен

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
+
    public static void main(String[] args) throws SQLException {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
